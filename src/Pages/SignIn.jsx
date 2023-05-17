e74a5a99a16f22087ecb099691e63770e2649b8f
Hira-Yuki <cjsdlf44@gmail.com>
2023-05-17 21:53:32 +0900

    로그인 페이지 작접 시작

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,22 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 
 function SignIn() {
+  const [nickName, setNickName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onNickNameChangeHandler = (event) => {
+    const inputNickName = event.target.value;
+    setNickName(inputNickName);
+  };
+
+  const onPasswordChangeHandler = (event) => {
+    const inputPassword = event.target.value;
+    setPassword(inputPassword);
+  };
+
   return (
     <StContiner>
       <h1>로그인</h1>
-      <label>ID</label>
-      <input type="email" placeholder="" />
+      <label>Nickname</label>
+      <input type="text" value={nickName} placeholder="Type your Nickname" onChange={onNickNameChangeHandler}/>
       <label>비밀번호</label>
-      <input type="password" placeholder="" />
+      <input
+        type="password"
+        value={password}
+        placeholder="Type your Password"
+        onChange={onPasswordChangeHandler}
+      />
       <div>
         <StBtn type="submit">로그인</StBtn>
-        <StBtn type="button">회원가입</StBtn>
-        <StBtn type="button">뒤로가기</StBtn>
+        <Link to={"/signup"}>
+          <StBtn type="button">회원가입</StBtn>
+        </Link>
+        <Link to={"/"}>
+          <StBtn type="button">뒤로가기</StBtn>
+        </Link>
       </div>
     </StContiner>
   );
 }
-
 export default SignIn;
 
 const StContiner = styled.div`
