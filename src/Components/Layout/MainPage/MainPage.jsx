00bf76c3cb19529a8d5b7e2fccaba00368d98535
jihyun29 <gi0129@naver.com>
2023-05-20 18:35:57 +0900

    동화조회,작성api

--- src/Components/Layout/MainPage/MainPage.jsx
+++ src/Components/Layout/MainPage/MainPage.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { styled } from "styled-components";
 import CreateModal from "../../feature/CreateStory/CreateModal";
 import CreateStory from "../../feature/CreateStory/CreateStory";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Body() {
   const [createStory, setCreateStory] = useState(false);
@@ -18,6 +19,30 @@ function Body() {
   const fairytales = useSelector((state) => state.fairyTale);
 
   console.log("fairytales??", fairytales);
+
+  // useState를 이용해서 네트워크 요청에 대한 상태를 관리합니다.
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // App.js에 들어오자마자 실행 할 함수를 작성합니다.
+  // 이 함수에서는 Posts를 서버에서 가져오는 기능입니다.
+  const getStories = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.get('/stories');
+      setPosts(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // useEffect를 이용해서 posts를 Fetching 합니다.
+  useEffect(() => {
+    getStories();
+  }, []);
 
   return (
     <StBodyBox>
@@ -37,56 +62,37 @@ function Body() {
         </StLi>
         {/*동화 리스트  */}
 
-    
-          {fairytales.stories.map((item) => {
-            return (
-              
-              <StLi key={item.storyId} style={{ backgroundImage: `url(${item.imageUrl})` }}>
-               <Link to={`/detail/${item.storyId}`} key={item.storyId}>
-                <div>
-                <h3>{item.title}</h3>
-                <p>{item.user}</p>
-                </div>
-                </Link> 
-              </StLi> 
-              
-            );
-          })}
-     
-
-     {fairytales.stories.map((item) => {
+        {fairytales.stories.map((item) => {
           return (
-            <StLi key={item.storyId}>
+            <StLi
+              key={item.storyId}
+              style={{ backgroundImage: `url(${item.imageUrl})` }}
+            >
               <Link to={`/detail/${item.storyId}`} key={item.storyId}>
-                <div style={{ backgroundImage: `url(${item.imageUrl})` }}></div>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.user}</p>
+                </div>
               </Link>
-              <div>
-                <h3>{item.title}</h3>
-                <p>{item.user}</p>
-              </div>
             </StLi>
           );
         })}
 
-
-{fairytales.stories.map((item) => {
-        return (
-          <StLi key={item.storyId}>
-            <Link to={`/detail/${item.storyId}`} key={item.storyId}>
-              <div
-                style={{ backgroundImage: `url(${item.imageUrl})` }}
-                onClick={() => {
-                  // handle click logic here if needed
-                }}
-              ></div>
-            </Link>
-            <div>
-              <h3>{item.title}</h3>
-              <p>{item.user}</p>
-            </div>
-          </StLi>
-        );
-      })}
+        {posts.map((item) => {
+          return (
+            <StLi
+              key={item.storyId}
+              style={{ backgroundImage: `url(${item.imageUrl})` }}
+            >
+              <Link to={`/detail/${item.storyId}`} key={item.storyId}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.user}</p>
+                </div>
+              </Link>
+            </StLi>
+          );
+        })}
 
         <StLi>
           <div>어린왕자</div>
@@ -132,7 +138,6 @@ const StLi = styled.li`
   height: 150px;
   border: 2px solid blue;
 `;
-
 
 const CreateBox = styled.div`
   width: 100%;
--- src/Components/feature/CreateStory/CreateStory.jsx
+++ src/Components/feature/CreateStory/CreateStory.jsx
@@ -2,22 +2,25 @@ import { styled } from "styled-components";
 import { addFairytale } from "../../../redux/modules/fairytale";
 import { useDispatch } from "react-redux";
 import React, { useState } from 'react';
+import { AuthApi } from "../../../shared/Api";
 
 
-function CreateStory(props) {
+function CreateStory({close, setPosts, posts }) {
 
-  const { close } = props;
+ 
+
+  // const { close } = props;
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [imageUrl, setImagUrl] = useState("");
+  // const titleChangeHandler = (event) => {
+  //   setTitle(event.target.value);
+  // };
 
-  const titleChangeHandler = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const contentChangeHandler = (event) => {
-    setContent(event.target.value);
-  };
+  // const contentChangeHandler = (event) => {
+  //   setContent(event.target.value);
+  // };
 
 
 
@@ -34,8 +37,7 @@ function CreateStory(props) {
       storyId: Date.now(),
       title,
       content,
-      imageUrl: imgFile,
-     
+      imageUrl,
     }));    
     close();
   };
@@ -48,30 +50,70 @@ function CreateStory(props) {
 
   //이미지 파일
 
-const [imgFile, setImgFile] = useState("");
 
-console.log(imgFile)
+
+console.log(imageUrl)
 
 
 const handleImageUrlChange = (event) => {
     
    
-  setImgFile(event.target.value);
+  setImagUrl(event.target.value);
     };
+
+
+
+ const [post,setPost] = useState({
+    title:"",
+    content:"",
+    imageUrl:"",
+  });
+
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmitPostHandler = async (event) =>{
+    event.preventDefault();
+    try{
+      setIsLoading(true);
+      const res = await AuthApi.postStories();
+      setPosts([...posts, res]);
+    } catch (error) {
+      // Error가 발생한다면, Alert를 띄움
+      alert("Error가 발생했습니다.");
+      setError(error);
+    } finally {
+      // 요청이 실패를 해도, 성공을 해도 실행되는 부분.
+      // 로딩상태를 false로 변경해준다.
+      setIsLoading(false);
+    }
+  }
+
+  
+  // Input 상태관리
+  const onChangeInputHandler = (event) => {
+    const { name, value } = event.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
+  };
+
+
 
 
   return ( 
     <StInputContiner>
-      <StInputBox>
-        제목 : <input type="text" value={title} onChange={titleChangeHandler}/>
-        첫문장 : <input type="text" value={content} onChange={contentChangeHandler} />
+      <StInputBox onSubmit={onSubmitPostHandler}>
+        제목 : <input type="text" name="title" onChange={onChangeInputHandler}/>
+        첫문장 : <input type="text" name="content" onChange={onChangeInputHandler} />
         표지 이미지 url: 
-        <input type="text" value={imgFile} onChange={handleImageUrlChange}
+        <input type="text" name="imageUrl" onChange={handleImageUrlChange}
         />
-        <button onClick={clickCreatstory}  >저장</button>
+        <button onClick={clickCreatstory} >{isLoading ? "저장중" : "저장"}</button>
       </StInputBox> 
       <StPreviewBox>
-        {<img src={imgFile} alt="이미지 미리보기" />}
+        {<img src={imageUrl} alt="이미지 미리보기" />}
     </StPreviewBox>
     </StInputContiner>
   );
